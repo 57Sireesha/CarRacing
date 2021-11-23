f669c53f5bf8a956d444e882e0b41ed104a59f83
57Sireesha <94772299+57Sireesha@users.noreply.github.com>
2021-11-23 20:01:14 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,24 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+
+  
+ 
+
+  //Bp
+  
+
+ 
+
+  //Bp
+  static getPlayersInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", data => {
+      allPlayers = data.val();
+    });
+  }
+}
